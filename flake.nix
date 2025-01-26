77fdb97763e6e1d2810552fe84c1d3fc65890bb2
Cenk Uluisik <cenk.uluisik@googlemail.com>
2025-01-26 01:00:16 +0100

    nixfmt

--- flake.nix
+++ flake.nix
@@ -1,38 +1,39 @@
 {
   description = "A mess of NixOS configs";
 
-    inputs = {
+  inputs = {
 
-        # nix packages version
-        nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nix packages version
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-    };
+  };
 
-  outputs = inputs@{ nixpkgs, ... }: rec {
+  outputs =
+    inputs@{ nixpkgs, ... }:
+    rec {
 
-    # config with nixos-system
-    nixosConfigurations.opi5b =
-        nixpkgs.lib.nixosSystem {
+      # config with nixos-system
+      nixosConfigurations.opi5b = nixpkgs.lib.nixosSystem {
 
         system = "x86_64-linux";
 
         # child inherit nixpkss
         specialArgs = {
-            inherit nixpkgs;
+          inherit nixpkgs;
         };
 
         modules = [
-            {
-                # target system
-                nixpkgs.crossSystem.config = "aarch64-unknown-linux-gnu";
-            }
-            # nixos-system
-            ./orangepi5b.nix
+          {
+            # target system
+            nixpkgs.crossSystem.config = "aarch64-unknown-linux-gnu";
+          }
+          # nixos-system
+          ./orangepi5b.nix
         ];
+      };
+
+      # build with "nix build #.opi5b"
+      opi5b = nixosConfigurations.opi5b.config.system.build.sdImage;
+
     };
-
-    # build with "nix build #.opi5b"
-    opi5b = nixosConfigurations.opi5b.config.system.build.sdImage;
-
-  };
 }
--- orangepi5b.nix
+++ orangepi5b.nix
@@ -1,84 +1,95 @@
-{ pkgs, nixpkgs, lib, ... }:
+{
+  pkgs,
+  nixpkgs,
+  lib,
+  ...
+}:
 {
 
-    imports = [
-        "${nixpkgs}/nixos/modules/installer/sd-card/sd-image.nix"
+  imports = [
+    "${nixpkgs}/nixos/modules/installer/sd-card/sd-image.nix"
+  ];
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  nix.settings = {
+    experimental-features = [
+      "nix-command"
+      "flakes"
+    ];
+  };
+
+  hardware.deviceTree.name = "rockchip/rk3588s-orangepi-5b.dtb";
+
+  boot = {
+    # fix zfs broken module
+    supportedFilesystems = lib.mkForce [
+      "vfat"
+      "fat32"
+      "exfat"
+      "ext4"
+      "btrfs"
     ];
 
-    boot.kernelPackages = pkgs.linuxPackages_latest;
-
-    nix.settings = {
-        experimental-features = ["nix-command" "flakes"];
+    loader = {
+      grub.enable = false;
+      generic-extlinux-compatible.enable = true;
     };
+  };
 
-    hardware.deviceTree.name = "rockchip/rk3588s-orangepi-5b.dtb";
+  hardware.graphics.enable = true;
 
-    boot = {
-        # fix zfs broken module
-        supportedFilesystems = lib.mkForce [
-            "vfat"
-            "fat32"
-            "exfat"
-            "ext4"
-            "btrfs"
-        ];
+  environment.systemPackages = with pkgs; [
+    git # used by nix flakes
+    curl
+    openssl
+  ];
 
-        loader = {
-            grub.enable = false;
-            generic-extlinux-compatible.enable = true;
-        };
+  # Enable the OpenSSH daemon.
+  services.openssh = {
+    enable = lib.mkDefault true;
+    settings = {
+      X11Forwarding = lib.mkDefault true;
+      PasswordAuthentication = lib.mkDefault true;
     };
+    openFirewall = lib.mkDefault true;
+  };
 
-    hardware.graphics.enable = true;
-
-    environment.systemPackages = with pkgs; [
-        git # used by nix flakes
-        curl
-        openssl
+  # Define user
+  users.users.cenk = {
+    hashedPassword = "$7$CU..../....fl8o80QpEM2Ro0B.E5MOF1$j2/9oN2UCciyxJxpaZE1Ta.V1ncHOHwb.W8mQ6C3Bj/"; # "test"
+    isNormalUser = true;
+    home = "/home/cenk";
+    description = "Cenk";
+    extraGroups = [
+      "wheel"
+      "networkmanager"
     ];
+  };
 
-    # Enable the OpenSSH daemon.
-    services.openssh = {
-        enable = lib.mkDefault true;
-        settings = {
-            X11Forwarding = lib.mkDefault true;
-            PasswordAuthentication = lib.mkDefault true;
-        };
-        openFirewall = lib.mkDefault true;
-    };
+  sdImage = {
+    compressImage = true;
 
-	# Define user
-	users.users.cenk = {
-        hashedPassword = "$7$CU..../....fl8o80QpEM2Ro0B.E5MOF1$j2/9oN2UCciyxJxpaZE1Ta.V1ncHOHwb.W8mQ6C3Bj/"; # "test"
-        isNormalUser  = true;
-        home  = "/home/cenk";
-        description  = "Cenk";
-        extraGroups  = [ "wheel" "networkmanager" ];
-	};
+    # Gap in front of the /boot/firmware partition, in mebibytes (1024×1024 bytes).
+    # Can be increased to make more space for boards requiring to dd u-boot SPL before actual partitions.
+    firmwarePartitionOffset = 32;
+    firmwarePartitionName = "BOOT";
+    firmwareSize = 200; # MiB
 
-    sdImage = {
-        compressImage = true;
+    populateFirmwareCommands = ''
+      mkdir -p ./firmware
+    '';
 
-        # Gap in front of the /boot/firmware partition, in mebibytes (1024×1024 bytes).
-        # Can be increased to make more space for boards requiring to dd u-boot SPL before actual partitions.
-        firmwarePartitionOffset = 32;
-        firmwarePartitionName = "BOOT";
-        firmwareSize = 200; # MiB
+    populateRootCommands = ''
+      mkdir -p ./files/boot
+    '';
 
-        populateFirmwareCommands = ''
-            mkdir -p ./firmware
-        '';
-
-        populateRootCommands = ''
-            mkdir -p ./files/boot
-        '';
-
-        # ???
-        # image location(sector): 0x40 - u-boot.bin.
-        postBuildCommands = ''
-            # places the U-Boot image at block first at block 64 (0x40)
-            dd if=${pkgs.ubootOrangePi5}/u-boot-rockchip.bin of=$img seek=64 conv=notrunc
-        '';
-    };
-    system.stateVersion = "22.11";
+    # ???
+    # image location(sector): 0x40 - u-boot.bin.
+    postBuildCommands = ''
+      # places the U-Boot image at block first at block 64 (0x40)
+      dd if=${pkgs.ubootOrangePi5}/u-boot-rockchip.bin of=$img seek=64 conv=notrunc
+    '';
+  };
+  system.stateVersion = "22.11";
 }
